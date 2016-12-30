dad33edd3a592352c9205459d277c181a9f78191
staug <tangilemoine@gmail.com>
2016-12-30 08:26:02 +0100

    Created constant file

---
+++ constants.py
@@ -0,0 +1,11 @@
+# This file holds all the constants for all the game.
+# Contrary to settings, these are things that should not be changed.
+
+# Utilities - class Publisher
+PUBLISHER_ALL = "ALL"
+PUBLISHER_CAT_LOG = "LOG"
+PUBLISHER_CAT_FIGHT = "FIGHT"
+
+# Tilemap - Tiles
+
+# Item
